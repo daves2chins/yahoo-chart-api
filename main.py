b45b1f7913904c06bcd3729614fc50cb54c5e7cb
daves2chins <daves2chins@gmail.com>
2025-11-11 17:09:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,8 @@ def chart():
     return send_file(buf, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
