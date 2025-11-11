f0a7be24d2480b2ce5d8cf8e07bfc76f60010eeb
daves2chins <daves2chins@gmail.com>
2025-11-11 17:27:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def chart():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 
